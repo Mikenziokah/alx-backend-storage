726a51c7f6eabe8a388b6099dd18d22af1437c54
Mikenziokah <mikenziokah@gmail.com>
2024-03-26 12:49:40 -0700

    update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" function that changes all topics of a school
+    document based on the name
+"""
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changing school documents based on name
+    """
+    query = {name: "name"}
+    new_names = {"$set": {"topics": topics}}
+    mongo_collection.update_many(query, new_names)
