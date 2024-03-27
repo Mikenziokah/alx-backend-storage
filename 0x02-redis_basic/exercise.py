067f65ad30770f3fa89b87c1874db1da7504d797
Mikenziokah <mikenziokah@gmail.com>
2024-03-27 19:14:52 +0000

    decode

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,9 +3,10 @@
 """
 
 import redis
-from typing import Union
+from typing import Union, Callable
 from uuid import uuid4
 import uuid
+import sys
 
 class Cache:
     def __init__(self):
@@ -18,3 +19,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def decode_utf8(b: bytes) -> str:
+        """ Decodes
+        """
+        return b.decode('utf-8') if type(b) == bytes else b
+
+
+    def replay(method: Callable):
+        """ Replay method
+        """
+    key = method.__qualname__
+    i = "".join([key, ":inputs"])
+    o = "".join([key, ":outputs"])
+    count = method.__self__.get(key)
+    i_list = method.__self__._redis.lrange(i, 0, -1)
+    o_list = method.__self__._redis.lrange(o, 0, -1)
+    queue = list(zip(i_list, o_list))
+    print(f"{key} was called {decode_utf8(count)} times:")
+    for k, v, in queue:
+        k = decode_utf8(k)
+        v = decode_utf8(v)
+        print(f"{key}(*{k}) -> {v}")
